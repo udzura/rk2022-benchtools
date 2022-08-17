367184555c284bddfeed59272ad21e99bdeacf05
Uchio Kondo <udzura@udzura.jp>
2022-08-17 21:53:41 +0900

    log10?

--- rbstr.rb
+++ rbstr.rb
@@ -5,6 +5,18 @@ prog = <<PROG
 BPF_HISTOGRAM(dist);
 BPF_HASH(start, u32);
 
+static u32 log10(u64 value) {
+  if (u64 == 0) { return 0; }
+  u32 log = 0;
+  for (int i = 0; i < 32; i++) {
+    value = value / 10;
+    if (value == 0 )
+      return log;
+    log += 1;
+  }
+  return log;
+}
+
 int rb_str_new_begin(void *ctx) {
   u64 ts = bpf_ktime_get_ns();
   u32 tid = bpf_get_current_pid_tgid();
@@ -22,7 +34,7 @@ int rb_str_new_return(void *ctx) {
     delta = bpf_ktime_get_ns() - *tsp;
     start.delete(&tid);
 
-    dist.increment(bpf_log2l(delta));
+    dist.increment(log10(delta));
   }
   return 0;
 }
@@ -42,10 +54,13 @@ b = BCC.new(text: prog)
 b.attach_uprobe(name: path, sym: "rb_str_new", fn_name: "rb_str_new_begin", pid: pid)
 b.attach_uretprobe(name: path, sym: "rb_str_new", fn_name: "rb_str_new_return", pid: pid)
 
+puts("collectiong data...")
+
 loop do
   begin
     sleep 1
   rescue Interrupt
+    puts
     break # pass
   end
 end
@@ -54,4 +69,4 @@ puts "elapsed time histogram"
 puts "~~~~~~~~~~~~~~"
 # print_etime_hist(b["dist"])
 
-b["dist"].print_log2_hist
+b["dist"].print_linear_hist
