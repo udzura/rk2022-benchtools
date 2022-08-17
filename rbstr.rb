e287910a6907299196b8764b911b0e0df40379a1
Uchio Kondo <udzura@udzura.jp>
2022-08-17 21:30:16 +0900

    Example2

---
+++ rbstr.rb
@@ -0,0 +1,37 @@
+prog = <<PROG
+int hola(void *ctx) {
+  bpf_trace_printk("Hello, World!\\n");
+  return 0;
+}
+
+int hola2(void *ctx) {
+  bpf_trace_printk("Return, World!\\n");
+  return 0;
+}
+PROG
+
+def usage
+  puts("USAGE: #{$0} [RUBY_PATH] [PID]")
+  exit()
+end
+
+usage if ARGV.size != 1 && ARGV.size != 2
+
+path = ARGV[0]
+pid = ARGV[1]&.to_i
+
+b = BCC.new(text: prog)
+b.attach_uprobe(name: path, sym: "rb_str_new", fn_name: "hola", pid: pid)
+b.attach_uretprobe(name: path, sym: "rb_str_new", fn_name: "hola2", pid: pid)
+
+loop do
+  begin
+    sleep 1
+  rescue Interrupt
+    break # pass
+  end
+end
+
+puts "elapsed time histogram"
+puts "~~~~~~~~~~~~~~"
+# print_etime_hist(b["dist"])
