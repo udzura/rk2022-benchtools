2b6cb1addd143297c9de01c7bb7bf48126fb3887
Uchio Kondo <udzura@udzura.jp>
2022-08-17 21:47:36 +0900

    log2hist

--- rbstr.rb
+++ rbstr.rb
@@ -2,13 +2,28 @@ require 'rbbcc'
 include RbBCC
 
 prog = <<PROG
-int hola(void *ctx) {
-  bpf_trace_printk("Hello, World!\\n");
+BPF_HISTOGRAM(dist);
+BPF_HASH(start, u32);
+
+int rb_str_new_begin(void *ctx) {
+  u64 ts = bpf_ktime_get_ns();
+  u32 tid = bpf_get_current_pid_tgid();
+
+  start.update(&tid, &ts);
   return 0;
 }
 
-int hola2(void *ctx) {
-  bpf_trace_printk("Return, World!\\n");
+int rb_str_new_return(void *ctx) {
+  u64 *tsp, delta;
+  u32 tid = bpf_get_current_pid_tgid();
+
+  tsp = start.lookup(&tid);
+  if (tsp != 0) {
+    delta = bpf_ktime_get_ns() - *tsp;
+    start.delete(&tid);
+
+    dist.increment(bpf_log2l(delta));
+  }
   return 0;
 }
 PROG
@@ -24,12 +39,11 @@ path = ARGV[0]
 pid = ARGV[1]&.to_i || -1
 
 b = BCC.new(text: prog)
-b.attach_uprobe(name: path, sym: "rb_str_new", fn_name: "hola", pid: pid)
-b.attach_uretprobe(name: path, sym: "rb_str_new", fn_name: "hola2", pid: pid)
+b.attach_uprobe(name: path, sym: "rb_str_new", fn_name: "rb_str_new_begin", pid: pid)
+b.attach_uretprobe(name: path, sym: "rb_str_new", fn_name: "rb_str_new_return", pid: pid)
 
 loop do
   begin
-    b.trace_print
     sleep 1
   rescue Interrupt
     break # pass
@@ -39,3 +53,5 @@ end
 puts "elapsed time histogram"
 puts "~~~~~~~~~~~~~~"
 # print_etime_hist(b["dist"])
+
+b["dist"].print_log2_hist
