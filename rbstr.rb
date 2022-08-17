fb4c308757e81b49dd9022f25a238d54bfa99593
Uchio Kondo <udzura@udzura.jp>
2022-08-17 22:03:04 +0900

    Show time

--- rbstr.rb
+++ rbstr.rb
@@ -10,7 +10,7 @@ static u32 log10(u64 value) {
   u32 log = 0;
   for (int i = 0; i < 32; i++) {
     value = value / 10;
-    if (value == 0 )
+    if (value == 0)
       return log;
     log += 1;
   }
@@ -44,6 +44,52 @@ def usage
   puts("USAGE: #{$0} [RUBY_PATH] [PID]")
   exit()
 end
+
+def stars(val, val_max, width)
+  DisplayHelper.stars(val, val_max, width)
+end
+
+def to_time(dur)
+  case dur
+  when (10**0)..(10**3)
+    "%dns" % dur
+  when (10**3)..(10**6)
+    "%dμs" % (dur / 1000)
+  when (10**6)..(10**9)
+    "%dms" % (dur / 1000 / 1000)
+  when (10**9)..(10**24)
+    "%ds"  % (dur / 1000 / 1000 / 1000)
+  end
+end
+
+def print_etime_hist(vals)
+      idx_max = 0
+      val_max = 0
+
+      vals.each do |i, v|
+        idx_max = i if i > idx_max
+        val_max = v if v > val_max
+      end
+
+      header = "     %-13s : count     distribution"
+      body = "        %-10d : %-8d |%-*s|"
+      stars = stars_max
+
+      if idx_max >= 0
+        puts(header % val_type);
+      end
+
+      (0...(idx_max + 1)).each do |i|
+        val = vals[i]
+        range = "%s .. %s" % [
+          to_time(10**i),
+          to_time(10**(i+1))
+        ]
+        
+        puts(body % [range, val, stars,
+                     stars(val, val_max, stars)])
+      end
+    end
 
 usage if ARGV.size != 1 && ARGV.size != 2
 
