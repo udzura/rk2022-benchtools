32b1153eb965febcf9ec194d0a7d19cd1239195c
Uchio Kondo <udzura@udzura.jp>
2022-08-23 22:00:29 +0900

    tidy

--- rbstr-3.rb
+++ rbstr-3.rb
@@ -5,7 +5,7 @@ prog = <<PROG
 #include <uapi/linux/ptrace.h>
 
 struct ruby_string_t {
-  char str[64];
+  char str[32];
 };
 
 struct ruby_value_t {
@@ -15,11 +15,11 @@ struct ruby_value_t {
   u64 ptr;
 };
 
-BPF_HASH(long, u32, u8);
-BPF_HASH(long1, u32, struct ruby_string_t);
-BPF_HASH(long2, u32, struct ruby_string_t);
-BPF_HASH(long3, u32, struct ruby_string_t);
-BPF_HASH(long4, u32, struct ruby_string_t);
+BPF_HASH(is_long, u32, u8);
+BPF_HASH(long1, u64, struct ruby_string_t, 16);
+BPF_HASH(long2, u64, struct ruby_string_t, 16);
+BPF_HASH(long3, u64, struct ruby_string_t, 16);
+BPF_HASH(long4, u64, struct ruby_string_t, 16);
 
 int rb_str_new_begin(struct pt_regs *ctx) {
   u32 tid = bpf_get_current_pid_tgid();
@@ -52,26 +52,25 @@ int rb_str_new_return(struct pt_regs *ctx) {
   u32 tid = bpf_get_current_pid_tgid();
   is = is_long.lookup(&tid);
   if (is != 0) {
-    u64 key = bpf_ktime_get_ns();
     struct ruby_value_t value = {0};
     struct ruby_string_t buf = {0};
     bpf_probe_read_user(&value, sizeof(buf), (struct ruby_value_t *)PT_REGS_RC(ctx));
+    bpf_probe_read_user(&buf.str, sizeof(buf.str), (char *)value.ptr);
+
+    u64 key = bpf_ktime_get_ns();
     if (*is == 1) {
-      bpf_probe_read_user(&buf.str, 50, (char *)value.ptr);
       long1.update(&key, &buf);
     }
     if (*is == 2) {
-      bpf_probe_read_user(&buf.str, sizeof(buf.str), (char *)value.ptr);
-      long1.update(&key, &buf);
+      long2.update(&key, &buf);
     }
     if (*is == 3) {
-      bpf_probe_read_user(&buf.str, sizeof(buf.str), (char *)value.ptr);
-      long1.update(&key, &buf);
+      long3.update(&key, &buf);
     }
     if (*is == 4) {
-      bpf_probe_read_user(&buf.str, sizeof(buf.str), (char *)value.ptr);
-      long1.update(&key, &buf);
+      long4.update(&key, &buf);
     }
+    is_long.delete(&tid);
   }
   
   return 0;
@@ -103,14 +102,17 @@ loop do
   end
 end
 
-puts "Sample strings 50 <= len < 200"
-b["long1"].each {|k, v| p v[0, 64].inspect }
+puts "*** Sample strings 50 <= len < 200"
+b["long1"].each {|k, v| p v[0, 32] }
 
-puts "Sample strings 200 <= len < 500"
-b["long1"].each {|k, v| p v[0, 64].inspect }
+puts
+puts "*** Sample strings 200 <= len < 500"
+b["long2"].each {|k, v| p v[0, 32] }
 
-puts "Sample strings 500 <= len < 1000"
-b["long1"].each {|k, v| p v[0, 64].inspect }
+puts
+puts "*** Sample strings 500 <= len < 1000"
+b["long3"].each {|k, v| p v[0, 32] }
 
-puts "Sample strings len >= 1000"
-b["long1"].each {|k, v| p v[0, 64].inspect }
+puts
+puts "*** Sample strings len >= 1000"
+b["long4"].each {|k, v| p v[0, 32] }
