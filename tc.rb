85355fd2a920ad757f718d1f49680c3489cf7cb3
Uchio Kondo <udzura@udzura.jp>
2022-08-23 20:31:30 +0900

    Fixes

--- tc.rb
+++ tc.rb
@@ -5,8 +5,8 @@ require 'ipaddr'
 
 begin
   require 'pycall/import'
-rescue e => LoadError
-  puts "${e.inspect}: needs pycall installed. run: gem install pycall"
+rescue LoadError => e
+  puts "#{e.inspect}: needs pycall installed. run: gem install pycall"
   exit 127
 end
 
@@ -14,6 +14,9 @@ unless iface = ARGV[0]
   puts("USAGE: #{$0} [IFACE]")
   exit 1
 end
+
+private_begin = IPAddr.new("192.168.0.0/32").to_i
+private_end   = IPAddr.new("192.168.255.255/32").to_i
 
 code = <<CODE
 // SPDX-License-Identifier: GPL-2.0+
@@ -24,22 +27,25 @@ code = <<CODE
 #include <bcc/proto.h>
 
 struct data_t {
-  i32 dest;
+  u32 dest;
 };
 
 BPF_PERF_OUTPUT(events);
 
 int on_egress(struct __sk_buff *skb) {
+  u8 *cursor = 0;
   struct ethernet_t *ethernet = cursor_advance(cursor, sizeof(*ethernet));
   if (ethernet->type != 0x0800) goto ret;
 
   struct ip_t *ip = cursor_advance(cursor, sizeof(*ip));
-  dest = ip->dst;
+  u32 dest = ip->dst;
   if (dest == 0) goto ret;
+  if (#{private_begin} <= dest && dest <= #{private_end})
+    goto ret;
 
-  struct data_t data = {0}
+  struct data_t data = {0};
   data.dest = dest;
-  events.perf_submit(ctx, &data, sizeof(data));
+  events.perf_submit(skb, &data, sizeof(data));
 ret:
   return TC_ACT_OK;
 }
@@ -52,14 +58,14 @@ end
 PyRoute2 = Py.pyroute2
 
 b = BCC.new(text: code)
-func = bpf.load_func("counter", BPF::SCHED_CLS)
+func = b.load_func("on_egress", BPF::SCHED_CLS)
 
-ip = PyRoute2::IPRoute.new
-ipdb = PyRoute2::IPDB.new
+ip = PyRoute2.IPRoute.new
+ipdb = PyRoute2.IPDB.new
 idx = ipdb.interfaces[iface].index
 ip.tc("add", "clsact", idx)
 
-ip.tc("add-filter", "bpf", idx, ":1", fd: func.fd, name: func.name,
+ip.tc("add-filter", "bpf", idx, ":1", fd: func[:fd], name: func[:name],
       parent: "ffff:fff3", classid: 1, direct_action: true)
 
 at_exit {
@@ -69,8 +75,8 @@ at_exit {
 
 b["events"].open_perf_buffer do |_cpu, data, _size|
   event = b["events"].event(data)
-  ip = IPAddr.new event.dest, Socket::AF_INET
-  puts "EGRESS IP: #{ip}"
+  got = IPAddr.new event.dest, Socket::AF_INET
+  puts "EGRESS IP: #{got}"
 end
 
 loop do
