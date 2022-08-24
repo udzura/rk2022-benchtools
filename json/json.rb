186c484e2ac5b51b5694ae190353ceb44c36520a
Uchio Kondo <udzura@udzura.jp>
2022-08-24 21:37:12 +0900

    COunt

--- json/json.rb
+++ json/json.rb
@@ -1,10 +1,10 @@
 require 'benchmark'
 require 'json'
 
-N = 10000
+N = 50000
 # M = 10
 
-Benchmark.bmbm do |x|
+Benchmark.bm do |x|
   SIMPLE = %q({"Hello": "world", "lireral": true, "numeric": 1})
   x.report("json obj") { N.times { JSON.parse(SIMPLE) } }
 end
--- json/ruston.rb
+++ json/ruston.rb
@@ -3,9 +3,9 @@ require 'ruston'
 
 # N = 100000
 # M = 10
-N = 10000
+N = 50000
 
-Benchmark.bmbm do |x|
+Benchmark.bm do |x|
   SIMPLE = %q({"Hello": "world", "lireral": true, "numeric": 1})
   r = Ruston.new
   x.report("ruston obj") { N.times { r.parse(SIMPLE) } }
