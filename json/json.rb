c4f648332d4c6fec022ca4c03352e124ae8b190a
Uchio Kondo <udzura@udzura.jp>
2022-09-05 21:29:48 +0900

    C mode

--- json/json.rb
+++ json/json.rb
@@ -1,7 +1,12 @@
 require 'benchmark'
 require 'json'
 
-N = 10000
+if ENV['C']
+  puts $$
+  $stdin.gets
+end
+
+N = ENV['N']&.to_i || 10000
 Benchmark.bm do |x|
   SIMPLE = %q({"Hello": "world", "lireral": true, "numeric": 1})
   x.report("json obj") { N.times { JSON.parse(SIMPLE) } }
--- json/ruston.rb
+++ json/ruston.rb
@@ -3,7 +3,7 @@ require 'ruston'
 
 # N = 100000
 # M = 10
-N = 10000
+N = ENV['N']&.to_i || 10000
 
 Benchmark.bm do |x|
   SIMPLE = %q({"Hello": "world", "lireral": true, "numeric": 1})
