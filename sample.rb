7bc85a7c9e67c2eec5350a97224bef669dbb8418
Uchio Kondo <udzura@udzura.jp>
2022-09-07 23:40:35 +0900

    Add new simple sample

--- sample.rb
+++ sample.rb
@@ -1,12 +1,11 @@
 require 'rbbcc'; include RbBCC
 
 code = <<CLANG
-#include <uapi/linux/ptrace.h>
-BPF_ARRAY(dist, u32, 1)
-int func(struct pt_regs *ctx) {
+BPF_ARRAY(dist, u64, 1);
+int func(void *ctx) {
   u32 key = 0;
-  u32 *count = dist.lookup(&key);
-  if (count) *count++;
+  u64 *count = dist.lookup(&key);
+  if (count) (*count)++;
   return 0;
 }
 CLANG
@@ -14,7 +13,7 @@ CLANG
 b = BCC.new(text: code)
 b.attach_kprobe(event: b.get_syscall_fnname("execve"), fn_name: "func")
 loop do
-  sleep 10;
-  puts "execve count in 10s: #{b['dist'][0]}"
-  b['dist'][0] = 0
+  sleep 3;
+  puts "#{Time.now} execve count in 3s: #{b['dist'].to_a[0]}"
+  b['dist'].clear
 end
